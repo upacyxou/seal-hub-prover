1f5a0879a2c723f8341ddb0dab0c972b90533157
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-11-22 12:22:31 -0800

    fix aws install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,9 @@ sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 # Add Docker official GPG key
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 # Add Docker repository to dpkg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # Refresh package list once again after adding new repo
 sudo apt-get update
 # Install Docker and required packages
